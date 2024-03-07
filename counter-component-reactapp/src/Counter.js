6f8966c43af81d097774ffef9238ae801a6d2da2
Diego-Cano <diegocano_1999@hotmail.com>
2024-03-06 16:08:06 -0800

    My assignment

---
+++ counter-component-reactapp/src/Counter.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import './Counter.css'; 
+
+function Counter() {
+    const [count, setCount] = useState(0);
+
+    const increment = () => setCount(count + 1);
+
+    const incrementAfterDelay = () => {
+        setTimeout(() => {
+            setCount(prevCount => prevCount + 1);
+        }, 2000);
+    };
+
+    const incrementTwice = () => {
+        setCount(count + 1);
+        setCount(count + 1);
+        setCount(prevCount => prevCount + 1);
+        
+    };
+
+    const correctIncrementTwice = () => {
+        setCount(prevCount => prevCount + 2);
+    };
+
+    return (
+        <div className="counter">
+            <h2>Count: {count}</h2>
+            <button onClick={increment}>Increment</button>
+            <button onClick={incrementAfterDelay}>Increment After Delay</button>
+            <button onClick={incrementTwice}>Increment Twice</button>
+            <button onClick={correctIncrementTwice}>Correct Increment Twice</button>
+        </div>
+    );
+}
+
+export default Counter;
